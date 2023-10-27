1b3b2c014db7d0cdd2eecbeae39c3fa31324a9b7
ivanharvard <144486839+ivanharvard@users.noreply.github.com>
2023-10-27 00:01:20 -0400

    Updated main.py
    Updated compute_score function.

--- assignment1/main.py
+++ assignment1/main.py
@@ -2,6 +2,7 @@
 import json
 import sys
 import os
+import math
 
 INPUT_FILE = 'testdata.json' # Constant variables are usually in ALL CAPS
 
@@ -16,9 +17,71 @@ class User:
 
 # Takes in two user objects and outputs a float denoting compatibility
 def compute_score(user1, user2):
-    # YOUR CODE HERE
-    return 0
+    # Initialize scores to some constant (Subtracting = more similar, Adding = less similar)
+    user1_score = 70
+    user2_score = 70
 
+    # If user1 likes user2's gender, we'll say they're more similar by subtracting
+    if user2.gender in user1.preferences:
+        user1_score -= 5
+    else:
+        user1_score += 60
+
+    if user1.gender in user2.preferences:
+        user2_score -= 5
+    else:
+        user2_score += 60
+
+    # Calculate the difference between their grad years
+    grad_year_difference = abs(user1.grad_year - user2.grad_year)
+
+    # The closer they are in grad year, the more compatible they will be
+    if grad_year_difference >= 3:
+        user1_score += 10
+        user2_score += 10
+    elif grad_year_difference == 2:
+        user1_score -= 3
+        user2_score -= 3
+    elif grad_year_difference == 3:
+        user1_score -= 10
+        user2_score -= 10
+    else:
+        user1_score -= 14
+        user2_score -= 14
+
+    for i in range(len(user1.responses)):
+        user1_response = user1.responses[i]
+        user2_response = user2.responses[i]
+
+        if user1_response == user2_response:
+            # Calculate the percentage of users who chose the same response for this question
+            same_response_percentage = sum(
+                1 for user in users if user.responses[i] == user1_response
+                ) / len(users)
+
+            # For every answer that matched, subtract a weight that is based on the percentage of people who answered the same way
+            weight = 1 / same_response_percentage
+            user1_score -= weight
+            user2_score -= weight
+
+    # If the scores become negative, set them to 0.
+    if user1_score < 0:
+        user1_score = 0
+
+    if user2_score < 0:
+        user2_score = 0
+
+    # Assume user1_score and user2_score are perpendicular vectors, and find vector sum
+    weighted_sum = math.sqrt(user1_score**2 + user2_score**2)
+
+    # Max score (polar opposites) will be 70 + 60 + 10 = 140.
+    # If both players get 135, their vector sum will be 140 * math.sqrt(2).
+    # Our final score should be a value between 1 and 0, 1 meaning most similar.
+    # weighted_sum should always be < or = the max score
+
+    final_score = 1 - (weighted_sum) / (140 * math.sqrt(2))
+
+    return final_score
 
 if __name__ == '__main__':
     # Make sure input file is valid
